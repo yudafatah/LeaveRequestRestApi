ce7cedd2cafafe66e0890a8b99decad8eb9625bc
yudafatah <yudafatah@gmail.com>
2019-01-07 08:27:17 +0700

    fix create leave request including upload photo

--- src/main/java/com/mii/leaverequestmaven/controller/LeaveRequestController.java
+++ src/main/java/com/mii/leaverequestmaven/controller/LeaveRequestController.java
@@ -13,6 +13,7 @@ import com.mii.leaverequestmaven.model.LeaveRequestType;
 import com.mii.leaverequestmaven.model.ServiceResponse;
 import com.mii.leaverequestmaven.tools.EncodeFile;
 import com.mii.leaverequestmaven.tools.countDate;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
@@ -67,21 +68,6 @@ public class LeaveRequestController {
         lrList.removeAll(lrList);
         for (LeaveRequest leaveRequest : lrdao.findAll()) {
             if (leaveRequest.getEmployeeId().getEmployeeId() == Integer.parseInt(lrid)) {
-//                LeaveRequest lr = new LeaveRequest();
-//                lr.setLrId(leaveRequest.getLrId());
-//                lr.setRequestDate(leaveRequest.getRequestDate());
-//                lr.setStartDate(leaveRequest.getStartDate());
-//                lr.setEndDate(leaveRequest.getEndDate());
-//                lr.setLrDuration(leaveRequest.getLrDuration());
-//                lr.setNoteRequest(leaveRequest.getNoteRequest());
-//                lr.setRequestStatus(leaveRequest.getRequestStatus());
-//                String img = null;
-//                byte[] bimg = leaveRequest.getImage();
-//                img = new String(bimg);
-//                lr.setImage(img.getBytes());
-//                lr.setNoteReject(leaveRequest.getNoteReject());
-//                lr.setEmployeeId(leaveRequest.getEmployeeId());
-//                lr.setTypeLrId(leaveRequest.getTypeLrId());
                 HashMap<String, String> lr = new HashMap<>();
                 lr.put("lrId", leaveRequest.getLrId() + "");
                 DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
@@ -125,28 +111,66 @@ public class LeaveRequestController {
     /*get all employees as manager*/
     @GetMapping("/leaverequestsmanager/{id}")
     public ResponseEntity<Object> getAllLeaveRequestManager(@PathVariable(value = "id") String lrid) {
-        leaverequestList.removeAll(leaverequestList);
+        lrList.removeAll(lrList);
         Employee e = edao.findOne(Integer.parseInt(lrid));
         for (Employee employee : e.getEmployeeList()) {
             for (LeaveRequest leaveRequest : employee.getLeaveRequestList()) {
-                leaverequestList.add(leaveRequest);
-                System.out.println("");
+                HashMap<String, String> lr = new HashMap<>();
+                lr.put("lrId", leaveRequest.getLrId() + "");
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+                String reqDate = format.format(leaveRequest.getRequestDate());
+                lr.put("requestDate", reqDate);
+                lr.put("startDate", leaveRequest.getStartDate().toString());
+                lr.put("endDate", leaveRequest.getEndDate().toString());
+                lr.put("lrDuration", leaveRequest.getLrDuration() + "");
+                lr.put("noteRequest", leaveRequest.getNoteRequest());
+                lr.put("requestStatus", leaveRequest.getRequestStatus());
+                String img = "x";
+                if (leaveRequest.getImage() != null) {
+                    byte[] bimg = leaveRequest.getImage();
+                    img = new String(bimg);
+                }
+                lr.put("image", img);
+                lr.put("noteReject", leaveRequest.getNoteReject());
+                lr.put("empName", leaveRequest.getEmployeeId().getEmployeeName());
+                lr.put("typeLr", leaveRequest.getTypeLrId().getTypeName());
+                lrList.add(lr);
             }
         }
-        ServiceResponse<List<LeaveRequest>> response = new ServiceResponse<>("success", leaverequestList);
+        ServiceResponse<List<HashMap<String, String>>> response = new ServiceResponse<>("success", lrList);
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 
-    /*get employee by empid*/
+    /*get Leave Request by lrId*/
     @GetMapping("/leaverequest/{id}")
-    public ResponseEntity<LeaveRequest> getLeaveRequestById(@PathVariable(value = "id") Integer lrid) {
-
+    public ResponseEntity<List<HashMap<String, String>>> getLeaveRequestById(@PathVariable(value = "id") Integer lrid) {
+        lrList.removeAll(lrList);
         LeaveRequest lr = lrdao.findOne(lrid);
+        HashMap<String, String> lr1 = new HashMap<>();
+        lr1.put("noteRequest", lr.getNoteRequest());
+        String img = "x";
+        if (lr.getImage() != null) {
+            byte[] bimg = lr.getImage();
+            img = new String(bimg);
+        }
+        lr1.put("image", img);
+        lr1.put("lrId", lr.getLrId() + "");
+        lr1.put("endDate", lr.getEndDate().toString());
+        lr1.put("noteReject", lr.getNoteReject());
+        lr1.put("empName", lr.getEmployeeId().getEmployeeName());
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        String reqDate = format.format(lr.getRequestDate());
+        lr1.put("requestDate", reqDate);
+        lr1.put("lrDuration", lr.getLrDuration() + "");
+        lr1.put("startDate", lr.getStartDate().toString());
+        lr1.put("requestStatus", lr.getRequestStatus());
+        lr1.put("typeLr", lr.getTypeLrId().getTypeName());
+        lrList.add(lr1);
 
         if (lr == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok().body(lr);
+        return ResponseEntity.ok().body(lrList);
     }
 
     /*update an lr by lrid*/
@@ -168,6 +192,31 @@ public class LeaveRequestController {
         lr.setRequestStatus("Approved");
         lr.setImage(lr.getImage());
         lr.setNoteReject(lr.getNoteReject());
+
+        LeaveRequest updateLeaveRequest = lrdao.save(lr);
+        return ResponseEntity.ok().body(updateLeaveRequest);
+
+    }
+
+    @PostMapping("/leaverequestreject/{id}/{noterej}")
+    public ResponseEntity<LeaveRequest> rejectLeaveRequest(@PathVariable(value = "id") String lrid,
+            @PathVariable(value = "noterej") String noterej) {
+
+        LeaveRequest lr = lrdao.findOne(Integer.parseInt(lrid));
+        if (lr == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        lr.setEmployeeId(lr.getEmployeeId());
+        lr.setTypeLrId(lr.getTypeLrId());
+        lr.setRequestDate(lr.getRequestDate());
+        lr.setStartDate(lr.getStartDate());
+        lr.setEndDate(lr.getEndDate());
+        lr.setLrDuration(lr.getLrDuration());
+        lr.setNoteRequest(lr.getNoteRequest());
+        lr.setRequestStatus("Rejected");
+        lr.setImage(lr.getImage());
+        lr.setNoteReject(noterej);
 
         LeaveRequest updateLeaveRequest = lrdao.save(lr);
         return ResponseEntity.ok().body(updateLeaveRequest);
@@ -199,11 +248,11 @@ public class LeaveRequestController {
 
     }
 
-    @PostMapping("/leaverequestcreate/{id}/{typelr}/{startdate}/{enddate}/{notereq}/{savePath}")
+    @PostMapping("/leaverequestcreate/{id}/{typelr}/{startdate}/{enddate}/{notereq}/{image}")
     public ResponseEntity<LeaveRequest> createLeaveRequest(@PathVariable(value = "id") String lrid,
             @PathVariable(value = "typelr") String typelr, @PathVariable(value = "startdate") String startdate,
             @PathVariable(value = "enddate") String enddate, @PathVariable(value = "notereq") String notereq,
-            @PathVariable(value = "savePath") String savePath) throws ParseException, IOException, Exception {
+            @PathVariable(value = "image") String image) throws ParseException, IOException, Exception {
 
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 //            Date requestDate = format.parse(reqDate);
@@ -212,11 +261,13 @@ public class LeaveRequestController {
         LocalDate localDate = LocalDate.now();
         Date reqdate = format.parse(localDate.toString());
         System.out.println(reqdate);
-//        String foto = EncodeFile.upload(savePath);
-//        byte[] b = foto.getBytes(Charset.forName("UTF-8"));
+        System.out.println("id : " + lrid + ", typelr : " + typelr + ", startdate : " + startdate + ", enddate : " + enddate + ", notereq : " + notereq+", image : "+image);
+        String savePath = "//Users//ustore//Yuda//bc21//SpringBoot//LeaveRequest//LeaveRequestJavaWebClient//web//images" + File.separator + image;
+        String foto = EncodeFile.upload(savePath);
+        byte[] b = foto.getBytes(Charset.forName("UTF-8"));
         int lrduration = countDate.countDays(startdate, enddate);
 
-        LeaveRequest lr = new LeaveRequest(null, reqdate, startDate, endDate, lrduration, notereq, "Waiting", null, null, new Employee(Integer.parseInt(lrid)), new LeaveRequestType(Integer.parseInt(typelr)));
+        LeaveRequest lr = new LeaveRequest(null, reqdate, startDate, endDate, lrduration, notereq, "Waiting", b, null, new Employee(Integer.parseInt(lrid)), new LeaveRequestType(Integer.parseInt(typelr)));
 
         LeaveRequest updateLeaveRequest = lrdao.save(lr);
         return ResponseEntity.ok().body(updateLeaveRequest);
